e0a8fe644eddfc4c1a8e1bd38b00764bceb0ad02
Alan Chen <alanchen1st@gmail.com>
2016-03-21 17:59:55 -0700

    Added output filename arguments

--- src/predict.py
+++ src/predict.py
@@ -4,18 +4,18 @@ import featurize
 import json
 import pickle
 
-def predict_submission(nn, scalerX, scalerY=None):
+def predict_submission(nn, scalerX, scalerY, readable_output, kaggle_output):
 	with open('../data/id_to_team.json') as f:
 		id_to_team = json.load(f)
 
 	with open('../data/stats.json') as f:
 		stats = json.load(f)
 
-	with open('../readable_submission.csv', 'w') as rs:
+	with open(readable_output, 'w') as rs:
 		writer_rs = csv.writer(rs, lineterminator='\n')
-		writer_rs.writerow(['Team 1, Team 2', 'Pred1', 'Pred2', '', 'Team 2, Team 1', 'Pred2', 'Pred1'])
+		writer_rs.writerow(['Team 1 Team 2', 'Pred1', 'Pred2', '', 'Team 2 Team 1', 'Pred2', 'Pred1'])
 
-		with open('../my_submission.csv', 'w') as ms:
+		with open(kaggle_output, 'w') as ms:
 			writer_ms = csv.writer(ms, lineterminator='\n')
 			writer_ms.writerow(['Id', 'Pred'])
 
@@ -52,11 +52,9 @@ def predict_submission(nn, scalerX, scalerY=None):
 
 					writer_ms.writerow([game_id, str(t1_activation[0])])
 					writer_rs.writerow([t1 + ' ' + t2, str(t1_activation[0]), str(t2_activation[0]), '', t2 + ' ' + t1, str(t2_activation[0]), str(t1_activation[0])])
-					# writer_rs.writerow([t1 + ' ' + t2, str(t1_activation[0]), str(t2_activation[0])])
-					# writer_rs.writerow([t2 + ' ' + t1, str(t2_activation[0]), str(t1_activation[0])])
 
 if __name__ == "__main__":
-	nn_filename = '../data/test_nn'
+	nn_filename = '../data/saved_nn'
 	nnObject = open(nn_filename,'r')
 	nn = pickle.load(nnObject)
 
@@ -66,4 +64,4 @@ if __name__ == "__main__":
 	scalerYObject = open(nn_filename + '_scalerY','r')
 	scalerY = pickle.load(scalerYObject)
 
-	predict_submission(nn, scalerX, scalerY)
+	predict_submission(nn, scalerX, scalerY, readable_output='../readable_submission.csv', kaggle_output='../my_submission.csv')
