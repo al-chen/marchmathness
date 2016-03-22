09a32fa5f1bb95744f25ab3b6c375c8f5e18791a
Alan Chen <alanchen1st@gmail.com>
2016-03-21 18:19:57 -0700

    Changed predict_submission to use predict_matchup

--- src/predict.py
+++ src/predict.py
@@ -28,24 +28,29 @@ def predict_submission(nn, scalerX, scalerY, readable_output, kaggle_output):
 					season, t1_id, t2_id = game_id.split('_')
 					t1, t2 = id_to_team[t1_id], id_to_team[t2_id]
 
-					x1, x2 = featurize.get_matchup_features(season, t1_id, t2_id, "N", id_to_team, stats)
-					if not x1 or not x2:
+					t1_activation, t2_activation = predict_matchup(nn, scalerX, scalerY, season, t1_id, t2_id, 'N', id_to_team, stats)
+					if not t1_activation or not t2_activation:
 						print 'Could not get features', t1, t2
 						break
 
-					x = []
-					x.append(x1)
-					x.append(x2)
-					x = scalerX.transform(x)
-					
-					x1, x2 = x
-					t1_activation, t2_activation = nn.activate(x1), nn.activate(x2)
-					if scalerY:
-						t1_activation, t2_activation = scalerY.inverse_transform(t1_activation), scalerY.inverse_transform(t2_activation)
+					# x1, x2 = featurize.get_matchup_features(season, t1_id, t2_id, "N", id_to_team, stats)
+					# if not x1 or not x2:
+					# 	print 'Could not get features', t1, t2
+					# 	break
 
-					sum_win_prob = t1_activation + t2_activation
-					t1_activation /= sum_win_prob
-					t2_activation /= sum_win_prob
+					# x = []
+					# x.append(x1)
+					# x.append(x2)
+					# x = scalerX.transform(x)
+					
+					# x1, x2 = x
+					# t1_activation, t2_activation = nn.activate(x1), nn.activate(x2)
+					# if scalerY:
+					# 	t1_activation, t2_activation = scalerY.inverse_transform(t1_activation), scalerY.inverse_transform(t2_activation)
+
+					# sum_win_prob = t1_activation + t2_activation
+					# t1_activation /= sum_win_prob
+					# t2_activation /= sum_win_prob
 
 					# print t1, t2
 					# print t1_activation, t2_activation
