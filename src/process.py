a74b16ae83ee7dff65ac1287555ccbf3804c1d12
Alan Chen <alanchen1st@gmail.com>
2016-03-23 14:51:06 -0700

    Added more team stats

--- src/process.py
+++ src/process.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-def get_team_stats(f='../data/kenpom.csv'):
+def get_kenpom_stats(f='../data/kenpom.csv'):
 	stats = {}
 	with open(f) as csvfile:
 		reader = csv.DictReader(csvfile)
@@ -32,13 +32,65 @@ def get_id_to_team_mappings(teams, spelling_to_id):
 	id_to_team = {v: k for k, v in team_to_id.items()}
 	return id_to_team
 
+def add_advanced_stats(stats, id_to_team):
+	stat_names = ['fgm', 'fga', 'fgm3', 'fga3', 'ftm', 'fta', 'oreb', 'dr', 'ast', 'to', 'stl', 'blk', 'pf',
+				  'opp_fgm', 'opp_fga', 'opp_fgm3', 'opp_fga3', 'opp_ftm', 'opp_fta', 'opp_oreb', 'opp_dr', 'opp_ast', 'opp_to', 'opp_stl', 'opp_blk', 'opp_pf']
+	results = ['../march-machine-learning-mania-2016-v2/TourneyDetailedResults.csv', '../march-machine-learning-mania-2016-v2/RegularSeasonDetailedResults.csv']
+	for f in results:
+		with open(f) as csvfile:
+			reader = csv.reader(csvfile)
+			headers = next(reader)
+			for row in reader:
+				season = row[0]
+
+				wteam, lteam = row[2], row[4]
+				wscore, lscore = row[3], row[5]
+
+				wteam_name, lteam_name = id_to_team[wteam], id_to_team[lteam]
+				
+				for team_name in [wteam_name, lteam_name]:
+					if team_name not in stats or season not in stats[team_name]:
+						continue
+					team_stats = stats[team_name][season]
+					if team_name == wteam_name:
+						fgm, fga, fgm3, fga3, ftm, fta, oreb, dr, ast, to, stl, blk, pf = [float(x) for x in row[8:21]]
+						opp_fgm, opp_fga, opp_fgm3, opp_fga3, opp_ftm, opp_fta, opp_oreb, opp_dr, opp_ast, opp_to, opp_stl, opp_blk, opp_pf = [float(x) for x in row[21:34]]
+					else:
+						fgm, fga, fgm3, fga3, ftm, fta, oreb, dr, ast, to, stl, blk, pf = [float(x) for x in row[21:34]]
+						opp_fgm, opp_fga, opp_fgm3, opp_fga3, opp_ftm, opp_fta, opp_oreb, opp_dr, opp_ast, opp_to, opp_stl, opp_blk, opp_pf = [float(x) for x in row[8:21]]
+
+					stat_nums = [fgm, fga, fgm3, fga3, ftm, fta, oreb, dr, ast, to, stl, blk, pf, 
+								 opp_fgm, opp_fga, opp_fgm3, opp_fga3, opp_ftm, opp_fta, opp_oreb, opp_dr, opp_ast, opp_to, opp_stl, opp_blk, opp_pf]
+
+					if 'counter' not in team_stats:
+						team_stats['counter'] = 0
+						for name in stat_names:
+							team_stats[name] = 0.0
+
+					team_stats['counter'] += 1
+					for name, num in zip(stat_names, stat_nums):
+						team_stats[name] += num
+
+	for team in stats.keys():
+		years = stats[team].keys()
+		for year in years:
+			team_stats = stats[team][year]
+			if 'counter' in team_stats:
+				ctr = team_stats['counter']
+				for stat in stat_names:
+					team_stats[stat] /= float(ctr)
+			# else:
+			# 	print team, year
+
 if __name__ == "__main__":
-	stats = get_team_stats('../data/kenpom.csv')
+	stats = get_kenpom_stats('../data/kenpom.csv')
 	teams = set(stats.keys())
 	spelling_to_id = check_spelling(teams)
 	id_to_team = get_id_to_team_mappings(teams, spelling_to_id)
-	
+
+	add_advanced_stats(stats, id_to_team)
+
 	with open('../data/id_to_team.json', 'w') as f:
 		json.dump(id_to_team, f)
-	with open('../data/stats.json', 'w') as f:
+	with open('../data/stats_advanced.json', 'w') as f:
 		json.dump(stats, f)
