bb11307e082134cbd74dcb67d8a1d1e8b1aa4466
Alan Chen <alanchen1st@gmail.com>
2017-03-12 21:51:43 -0700

    Added process_winrates from mov

--- src/predict.py
+++ src/predict.py
@@ -3,8 +3,38 @@ import numpy as np
 import featurize
 import json
 import pickle
+from marchine import MMNN
+from sklearn import preprocessing
 
-def predict_submission(nn, scalerX, scalerY, id_to_team, stats, readable_output, kaggle_output):
+def predict_matchup(mn, season, t1_id, t2_id, t1_loc):
+	x1, x2 = featurize.get_matchup_features(season, t1_id, t2_id, t1_loc, mn.id_to_team, mn.stats)
+	if not x1 or not x2:
+		print 'Could not get features', mn.id_to_team[t1_id], mn.id_to_team[t2_id]
+		print x1
+		print x2
+		return None, None
+
+	x = []
+	x.append(x1)
+	x.append(x2)
+	x = mn.scalerX.transform(x)
+	
+	x1, x2 = x
+	t1_activation, t2_activation = mn.nn.activate(x1), mn.nn.activate(x2)
+	if mn.mov_label and mn.scalerY is not None: # Margin of victory used as labels
+		t1_activation, t2_activation = mn.scalerY.inverse_transform(t1_activation.reshape(1,-1))[0], mn.scalerY.inverse_transform(t2_activation.reshape(1,-1))[0]
+		avg = (t1_activation - t2_activation) / 2.0
+		t1_activation, t2_activation = avg, -avg
+	else:
+		sum_win_prob = t1_activation + t2_activation
+		t1_activation /= sum_win_prob
+		t2_activation /= sum_win_prob
+
+	return t1_activation, t2_activation
+
+def predict_submission(mn_filename, readable_output, kaggle_output, submission):
+	mn = MMNN()
+	mn.load(mn_filename)
 	with open(readable_output, 'w') as rs:
 		writer_rs = csv.writer(rs, lineterminator='\n')
 		writer_rs.writerow(['Team 1 Team 2', 'Pred1', 'Pred2', '', 'Team 2 Team 1', 'Pred2', 'Pred1'])
@@ -13,16 +43,16 @@ def predict_submission(nn, scalerX, scalerY, id_to_team, stats, readable_output,
 			writer_ms = csv.writer(ms, lineterminator='\n')
 			writer_ms.writerow(['Id', 'Pred'])
 
-			with open('../march-machine-learning-mania-2016-v2/SampleSubmission.csv') as csvfile:
+			with open(submission) as csvfile:
 				reader = csv.reader(csvfile)
 				headers = next(reader)
 
 				for row in reader:
 					game_id = row[0]
 					season, t1_id, t2_id = game_id.split('_')
-					t1, t2 = id_to_team[t1_id], id_to_team[t2_id]
+					t1, t2 = mn.id_to_team[t1_id], mn.id_to_team[t2_id]
 
-					t1_activation, t2_activation = predict_matchup(nn, scalerX, scalerY, season, t1_id, t2_id, 'N', id_to_team, stats)
+					t1_activation, t2_activation = predict_matchup(mn, season, t1_id, t2_id, 'N')
 					if not t1_activation or not t2_activation:
 						print 'Could not get features', t1, t2
 						break
@@ -33,45 +63,37 @@ def predict_submission(nn, scalerX, scalerY, id_to_team, stats, readable_output,
 					writer_ms.writerow([game_id, str(t1_activation[0])])
 					writer_rs.writerow([t1 + ' ' + t2, str(t1_activation[0]), str(t2_activation[0]), '', t2 + ' ' + t1, str(t2_activation[0]), str(t1_activation[0])])
 
-def predict_matchup(nn, scalerX, scalerY, season, t1_id, t2_id, t1_loc, id_to_team, stats):
-	x1, x2 = featurize.get_matchup_features(season, t1_id, t2_id, t1_loc, id_to_team, stats)
-	if not x1 or not x2:
-		print 'Could not get features', id_to_team[t1_id], id_to_team[t2_id]
-		print x1
-		print x2
-		return None, None
+def process_winrates(filename):
+	with open(filename) as csvfile:
+		reader = csv.reader(csvfile)
+		headers = next(reader)
+		matchups, y = [], []
+		for row in reader:
+			matchup, mov = row
+			matchups.append(matchup)
+			y.append(mov)
 
-	x = []
-	x.append(x1)
-	x.append(x2)
-	x = scalerX.transform(x)
-	
-	x1, x2 = x
-	t1_activation, t2_activation = nn.activate(x1), nn.activate(x2)
-	if scalerY:
-		t1_activation, t2_activation = scalerY.inverse_transform(t1_activation), scalerY.inverse_transform(t2_activation)
+		y = np.array(y).astype(float)
+		maxy, miny = max(y), min(y)
+		print maxy, miny
+		for i in range(len(y)):
+			if y[i] >= 0.0:
+				y[i] = y[i] / float(maxy) * 0.5 + 0.5
+			else:
+				y[i] = y[i] / float(abs(miny)) * 0.5 + 0.5
+		print max(y), min(y)
 
-	sum_win_prob = t1_activation + t2_activation
-	t1_activation /= sum_win_prob
-	t2_activation /= sum_win_prob
+		y = np.array(y).astype(float).reshape((len(y), 1))
+		mms = preprocessing.MinMaxScaler().fit(y)
+		winrates = mms.transform(y)
+		print np.average(winrates)
 
-	return t1_activation, t2_activation
-
+	with open('../asdf.csv', 'w') as f:
+		writer = csv.writer(f, lineterminator='\n')
+		writer.writerow(['Id', 'Pred'])
+		for matchup, wr in zip(matchups, winrates):
+			writer.writerow([matchup] + list(wr))
+		
 if __name__ == "__main__":
-	with open('../data/id_to_team.json') as f:
-		id_to_team = json.load(f)
-
-	with open('../data/stats_advanced.json') as f:
-		stats = json.load(f)
-
-	nn_filename = '../data/advanced_nn_100epochs'
-	nnObject = open(nn_filename,'r')
-	nn = pickle.load(nnObject)
-
-	scalerXObject = open(nn_filename + '_scalerX','r')
-	scalerX = pickle.load(scalerXObject)
-
-	scalerYObject = open(nn_filename + '_scalerY','r')
-	scalerY = pickle.load(scalerYObject)
-
-	predict_submission(nn, scalerX, scalerY, id_to_team, stats, readable_output='../readable_output_advanced.csv', kaggle_output='../my_submission.csv')
+	predict_submission('../data_2017/saved_nn', '../readable_output.csv', '../my_submission.csv', '../march-machine-learning-mania-2017/sample_submission.csv')
+	# process_winrates('../my_submission.csv')
